80675a87f2293a66e52da60631f479a8723d0cf9
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-11-06 17:29:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+  <PageHeader />
+  <PageMain />
+  <PageFooter />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+    name: "AppFooter",
+    data() {
+        return {
+        }
+    }
+}
+
+</script>
+
+<template></template>
+
+<style scoped></style>
