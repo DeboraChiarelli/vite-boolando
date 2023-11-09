92869b96c43db663494acc63e2f2880cc4aee13d
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-11-09 15:49:35 +0100

    Add - closeModal alla  x per chiusura modale

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,14 +13,20 @@ export default {
         return {
             store: store,
             open: false,
+            selectedCard: {}
         }
     },
     methods: {
 
     },
-    showModal() {
-        
-    }
+    showModal(card) {
+        this.selectedCard = card
+        this.open = true
+    },
+    closeModal() {
+        this.open = false
+        this.selectedCard = {}
+    },
     created() {
         axios.get("http://localhost:3000/products")
             .then(res => {
@@ -42,8 +48,50 @@ export default {
 
             </div>
         </section>
-
+        <div v-if="open" class="modal">
+            <div class="content">
+                <div>
+                    <p> {{ selectedCard.brand }}</p>
+                    <span @click="closeModal">&#10005;</span>
+                </div>
+                <div>
+                    <span>
+                        {{ selectedCard.name }}
+                    </span>
+                    <p>
+                        {{ selectedCard.price }}
+                    </p>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.modal::after {
+    content: '';
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 40;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal .content {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 50;
+    background-color: white;
+    border-radius: 20px;
+    padding: 20px;
+    width: 100%;
+    max-width: 500px;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+
+
+}
+</style>
